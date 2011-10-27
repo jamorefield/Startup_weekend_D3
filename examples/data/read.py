ffc5c2b630c1dae19d95838744d15c72dbb9b211
JAmorefield <john@askitecture.com>
2011-10-26 21:33:13 -0700

    this is the day

--- examples/data/read.py
+++ examples/data/read.py
@@ -3,23 +3,32 @@ from string import Template
 
 # Init the string template
 s = Template("""{"type":"Feature","id":$id,"geometry":{"type":"Point","coordinates":[$long,$lat]},
-    "properties":{"city":"$city","attendance":$attendance,"date":"$date"}},\n""")
+    "properties":{"city":"$city","attendance":$attendance,"date":"$date"}}""")
 
 # Init output file
-ofile = open("us-centroids.json", "w")
+ofile = open("swviz_data.json", "w")
 ofile.write("""{"type":"FeatureCollection","features":[\n""")
 
 filename = "swviz_data.csv"
 reader = csv.reader(open(filename, "r"))
 
+last_line_no = 0
+for row in reader:
+	last_line_no += 1
+
+print last_line_no
+reader = csv.reader(open(filename, "r"))
+
 try:
 	for row_num, row in enumerate(reader):
-		print row
 		if row_num != 0:
 			ofile.write(s.substitute(long=row[0], lat=row[1], city=row[2], attendance=row[3], date=row[4], id=row_num))
+		if row_num != last_line_no - 1 or row_num == 1:
+		 	ofile.write(",\n")
 except csv.Error, e:
     sys.exit("file %s, line %d: %s" % (filename, reader.line_num, e))
 
 ofile.write("]}")
 ofile.close()
+
 
